2b0ebe7769c6b0ebe432deb2f24024d0bf14c3ce
hai-an <1207783846@qq.com>
2022-11-07 20:01:43 +0800

    员工列表页面的基本布局和结构

--- src/views/employees/index.vue
+++ src/views/employees/index.vue
@@ -1,19 +1,49 @@
 <template>
   <div class="dashboard-container">
     <div class="app-container">
-      <h2>
-        员工
-      </h2>
+      <!-- 顶部栏 -->
+      <page-tools :show-before="true">
+        <span slot="before">共16条记录</span>
+        <template v-slot:after>
+          <el-button size="small" type="warning">导入execle</el-button>
+          <el-button size="small" type="danger">导出execle</el-button>
+          <el-button size="small" type="primary">新增员工</el-button>
+        </template>
+      </page-tools>
+      <!-- 表格 -->
+      <el-card>
+        <el-table border="">
+          <el-table-column label="序号" sortable="" />
+          <el-table-column label="姓名" sortable="" />
+          <el-table-column label="工号" sortable="" />
+          <el-table-column label="聘用形式" sortable="" />
+          <el-table-column label="部门" sortable="" />
+          <el-table-column label="入职时间" sortable="" />
+          <el-table-column label="账户状态" sortable="" />
+          <el-table-column label="操作" sortable="" fixed="right" width="280">
+            <template>
+              <el-button type="text" size="small">参看</el-button>
+              <el-button type="text" size="small">转正</el-button>
+              <el-button type="text" size="small">调岗</el-button>
+              <el-button type="text" size="small">离职</el-button>
+              <el-button type="text" size="small">角色</el-button>
+              <el-button type="text" size="small">删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+        <!-- 分页组件 -->
+        <el-row type="flex" justify="center" align="middle" style="height: 60px;">
+          <el-pagination
+            layout="prev, pager, next"
+          />
+        </el-row>
+      </el-card>
     </div>
   </div>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
 
-<style>
-
-</style>
+<style></style>
