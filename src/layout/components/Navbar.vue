c1f0c749a89f20e5cb357ab65e01b0d99b0a75ae
hai-an <1207783846@qq.com>
2022-11-15 19:51:47 +0800

    实现页面的全屏功能

--- src/layout/components/Navbar.vue
+++ src/layout/components/Navbar.vue
@@ -12,6 +12,8 @@
       <span class="breadBtn">VIP版</span>
     </div>
     <div class="right-menu">
+      <!--放置 全屏组件 -->
+      <screen-full class="right-menu-item" />
       <el-dropdown class="avatar-container" trigger="click">
         <div class="avatar-wrapper">
           <img v-imagerror="defaultImg" :src="staffPhoto" class="user-avatar">
@@ -125,7 +127,7 @@ export default {
       height: 100%;
       font-size: 18px;
       color: #5a5e66;
-      vertical-align: text-bottom;
+      vertical-align: middle;
 
       &.hover-effect {
         cursor: pointer;
